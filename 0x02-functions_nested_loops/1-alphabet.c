c89b718340fbd0693ab63d2acb83808fed57e477
Manar-Mansour <manar.abdelgalil2014@gmail.com>
2023-08-16 01:46:20 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
         char letter;
 
@@ -15,5 +15,9 @@ int main(void)
 
 	_putchar('\n');
 
-	return (0);
+}
+int main(void)
+{
+  print_alphabet();
+  return (0);
 }
