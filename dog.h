de19128da074877574175bd4881f1d5fa231075b
Manar-Mansour <manar.abdelgalil2014@gmail.com>
2023-09-11 19:07:36 -0700

    dog.h

--- dog.h
+++ dog.h
@@ -1,22 +1,35 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct dog - structure for a basic dog
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- * Description: data structure for dog
- */
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
+
+
+/**
+*dog_t - Typedef for struct dog
+*/
 typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
 #endif
