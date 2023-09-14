aceb4269534373d89ecb7070d40bc0ecbbba821d
Manar-Mansour <manar.abdelgalil2014@gmail.com>
2023-09-14 05:24:11 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -25,7 +25,7 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", (char) va_arg(ap, int));
+				printf("%c", (char) va_arg(ap, char));
 				break;
 			case 'i':
 				printf("%d", va_arg(ap, int));
